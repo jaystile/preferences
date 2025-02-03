875a779620799a3af00d5c62babdd93cd51ac296
Jason Stiles <99777116+stilesj-uchicago@users.noreply.github.com>
2025-02-03 14:55:33 -0500

    Adds tmux-yank for yanking to the clipboard

---
+++ setup/install_apt_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt update # get the latest packages
+
+sudo apt install -y \
+	xsel # used for tmux-yank to copy to clipboard \
---
+++ setup/install_tmux_plugins.sh
@@ -0,0 +1,4 @@
+# Installs tpm the Tmux Plugin Manager
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
