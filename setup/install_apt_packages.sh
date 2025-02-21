a8a05eaad75f43b5236b1ab0babbc8f42c5fe924
jaystile <142552281+pico-bolero@users.noreply.github.com>
2025-02-21 05:49:58 -0500

    adds psql

--- setup/install_apt_packages.sh
+++ setup/install_apt_packages.sh
@@ -4,16 +4,16 @@ sudo apt update # get the latest packages
 
 # nvim kick start dependencies
 sudo apt install -y \
-	ripgrep \
-	xsel \
-	fonts-noto-color-emoji
+    ripgrep \
+    xsel \
+    fonts-noto-color-emoji
 
 # pyenv dependencies
 sudo apt install -y \
-	tk-dev \
-	libpq-dev
-
+    tk-dev \
+    libpq-dev
 
 # personal preferences
 sudo apt install -y \
-	shellcheck 
+    postgresql-client \
+    shellcheck
