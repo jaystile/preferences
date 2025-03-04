5cae08e681d792bc70b570b9ab2faea605836b8b
jaystile <142552281+pico-bolero@users.noreply.github.com>
2025-03-04 08:24:17 -0500

    Adds npm apps to the path

---
+++ setup/configure_npm_apps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CMD=npm
+if ! command -v ${CMD} 2>&1 >/dev/null
+then
+    echo "${CMD} could not be found. Install ${CMD} with asdf."
+    exit 1
+fi
+
+# Installs tools into ~/node_modules/bin
+${CMD} install prettier -g
