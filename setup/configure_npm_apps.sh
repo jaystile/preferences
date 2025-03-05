8e78a8faad22b370c2badc30249bfc07e8f63933
jaystile <142552281+pico-bolero@users.noreply.github.com>
2025-03-05 10:37:09 -0500

    Fixes npm global path configuration

--- setup/configure_npm_apps.sh
+++ setup/configure_npm_apps.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 CMD=npm
-if ! command -v ${CMD} 2>&1 >/dev/null
-then
-    echo "${CMD} could not be found. Install ${CMD} with asdf."
-    exit 1
+if ! command -v ${CMD} 2>&1 >/dev/null; then
+  echo "${CMD} could not be found. Install ${CMD} with asdf."
+  exit 1
 fi
 
-# Installs tools into ~/node_modules/bin
 ${CMD} install prettier -g
 ${CMD} install jsonlint -g
