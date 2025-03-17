8708336c1d4e33c4482e07044ca4a0b7247ccd2d
jaystile <142552281+pico-bolero@users.noreply.github.com>
2025-03-17 07:18:35 -0400

    Adds graphviz to support plantuml

--- setup/install_apt_packages.sh
+++ setup/install_apt_packages.sh
@@ -4,19 +4,22 @@ sudo apt update # get the latest packages
 
 # nvim kick start dependencies
 sudo apt install -y \
-    ripgrep \
-    xsel \
-    fonts-noto-color-emoji
+  ripgrep \
+  xsel \
+  fonts-noto-color-emoji
 
 # pyenv dependencies
 sudo apt install -y \
-    tk-dev \
-    libpq-dev
+  tk-dev \
+  libpq-dev
 
 # phoenix framework dependencies
 supo apt install -y inotify-tools
 
+# plantuml dependencies
+sudo apt install -y graphviz
+
 # personal preferences
 sudo apt install -y \
-    postgresql-client \
-    shellcheck
+  postgresql-client \
+  shellcheck
